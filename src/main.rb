24901d22616345948974349a2fe80b8ef209fef6
MrBocch <jorgealberto1436@gmail.com>
2024-01-25 00:21:46 -0600

    added borders to table, code is so ugly, sorry future me

--- src/main.rb
+++ src/main.rb
@@ -90,34 +90,83 @@ def stats()
   rows = db.execute("SELECT * FROM act")
   db.close
 
-  floor = '─'
-  wall = '│'
-  tsign  = {:ut => '┴', :dt => '┬', :rt => '├', :lt => '┤', :cross => '┼'}
-  corner = {:tr => '┌', :tl => '┐', :br => '┘', :bl => '└'}
-
-  rpad = rows.max{|a, b| a[1].size <=> b[1].size}[1].length
-
-  id = "id".ljust(3, " ")
-  activity = "Activity".ljust(rpad, " ")
-
-  header = "#{id} #{wall} #{activity} #{wall} Time" 
-  # this wont work i have to insert the cross piece
-  lines = header.split("").map{|c| floor}.join("")
-
-  puts header
-
-  rows.each do |row|
-    id = row[0].to_s.ljust(3, " ")
-    act = row[1].ljust(rpad, " ") # so cool
-    h, m = secToHM(row[2])
-
-    # repeating my self alittle 
+  0.upto(rows.length() -1) do |i|
+    h, m = secToHM(rows[i][2])
     if h > 0
-      puts "#{id} #{wall} #{act} #{wall} #{h} Hours, #{m} Minutes"
+      rows[i][2] = "#{h} Hours, #{m} Minutes"
     else 
-      puts "#{id} #{wall} #{act} #{wall} #{m} Minutes"
+      rows[i][2] = "#{m} Minutes"
     end
   end
+
+  rpad_id = rows.max{|a, b| a[0].to_s.size <=> b[0].to_s.size}[0].to_s.length
+  rpad_id = [rpad_id, "id".length].max
+  rpad_act = rows.max{|a, b| a[1].size <=> b[1].size}[1].length
+  rpad_time = rows.max{|a, b| a[2].size <=> b[2].size}[2].length
+
+  id = "id".ljust(rpad_id, " ")
+  activity = "Activity".ljust(rpad_act, " ")
+  time = "Time".ljust(rpad_time, " ")
+ 
+  floor = '─'
+  wall = '│' 
+
+  id_col = "#{wall} #{id} #{wall}"
+  activity_col = "#{activity} #{wall}"
+  time_col = "#{time} #{wall}"
+
+  tsign  = {:ut => '┴', :dt => '┬', :rt => '├', :lt => '┤', :cross => '┼'}
+  corner = {:tr => '┌', :tl => '┐', :br => '┘', :bl => '└'}
+  # this must be the worst code i have ever written in my life
+  # sorrow to those that lay their eyes upon it
+  uheader = (id_col).split("").map{|c| floor}.join("")
+  uheader[0] = corner[:tr]
+  uheader[-1] = corner[:tl]
+
+  uheader += (activity_col).split("").map{|c| floor}.join("")
+  uheader[id_col.length-1] = tsign[:dt]
+  uheader += tsign[:dt]
+
+  uheader += (time_col).split("").map{|c| floor}.join("")
+  uheader += corner[:tl]
+
+  dheader = (id_col).split("").map{|c| floor}.join("")
+  dheader[0] = tsign[:rt]
+  dheader[-1] = tsign[:cross]
+
+  dheader += (activity_col).split("").map{|c| floor}.join("")
+  dheader += tsign[:cross]
+  
+  dheader += (time_col).split("").map{|c| floor}.join("")
+  dheader += tsign[:lt]
+
+  endtable = (id_col).split("").map{|c| floor}.join("")
+  endtable[0] = corner[:bl]
+  endtable[-1] = tsign[:ut]
+
+  endtable += (activity_col).split("").map{|c| floor}.join("")
+  endtable += tsign[:ut]
+
+  endtable += (time_col).split("").map{|c| floor}.join("")
+  endtable += corner[:br]
+
+  header = "#{id_col} #{activity_col} #{time_col}" 
+
+  # print the top part of table 
+  puts uheader 
+  puts header
+  puts dheader
+  # print part of table right below header
+
+  rows.each do |row|
+    id = row[0].to_s.ljust(rpad_id, " ")
+    act = row[1].ljust(rpad_act, " ") # so cool
+    time = row[2].ljust(rpad_time, " ")
+    # h, m = secToHM(row[2])
+
+    puts "#{wall} #{id} #{wall} #{act} #{wall} #{time} #{wall}"
+  end
+  puts endtable
 
 end
 
